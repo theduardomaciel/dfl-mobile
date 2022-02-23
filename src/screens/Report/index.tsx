50aec98cc4531b2ba47b4b44018473170844f46f
Eduardo Maciel <eduardomacielbr@hotmail.com>
2022-02-22 22:44:54 -0300

    Sistema que não deu certo

--- src/screens/Report/index.tsx
+++ src/screens/Report/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, StatusBar, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StatusBar, Text, Image, useWindowDimensions, ScrollView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import MapView, { PROVIDER_GOOGLE, Marker, Region } from "react-native-maps";
@@ -25,6 +25,11 @@ type Report = {
     resolved: boolean;
 }
 
+type TagsType = {
+    id: string;
+    title: string;
+}
+
 export function Report({ route, navigation }) {
     const report = route.params.item as Report;
 
@@ -38,6 +43,34 @@ export function Report({ route, navigation }) {
     }
 
     const suggestion = report.suggestion ? report.suggestion : "[nenhuma sugestão foi informada]"
+
+
+    let groups = [];
+    const tagGroups = JSON.parse(report.tags);
+    console.log(tagGroups)
+
+    const { height, width } = useWindowDimensions();
+    const [tags, setTags] = useState(Array)
+    useEffect(() => {
+        const tagsViewWidth = (parseInt(elements.subContainerWhite.width) / 100) * width
+        let tagsInArrayWidth = 0
+        for (const [key, tagGroup] of Object.entries(tagGroups)) {
+            for (const [key, tag] of Object.entries(tagGroup)) {
+                console.log(tag)
+                groups.push(
+                    <View onLayout={(event) => {
+                        const { x, y, width, height } = event.nativeEvent.layout;
+                        tagsInArrayWidth += width
+                    }} style={[styles.tag, { width: tagsInArrayWidth <= (tagsViewWidth / 2) ? "50%" : "25%" }]}>
+                        <Text style={styles.tagText}>{tag.title}</Text>
+                        <MaterialIcons name="done" size={18} color="white" />
+                    </View>
+                )
+            }
+        }
+        console.log(tagsInArrayWidth)
+        setTags(groups)
+    }, [])
 
     return (
         <View style={styles.container}>
@@ -92,9 +125,12 @@ export function Report({ route, navigation }) {
             </View>
             <BottomBar info={report.address} viewStyle={{ marginBottom: 15 }} />
             <SectionTitle title="Detalhes:" fontStyle={{ fontSize: 18 }} marginBottom={5} />
-            <View style={[elements.subContainerWhite, { flex: 1 }]}>
+            <ScrollView style={[elements.subContainerWhite, styles.tagsContainer]}>
                 <TextButton title="+" buttonStyle={{ paddingHorizontal: 8, paddingVertical: 1, borderRadius: 8, position: "absolute", right: 0, top: -30 }} />
-            </View>
+                <View style={styles.gridContainer}>
+                    {tags}
+                </View>
+            </ScrollView>
             <LinearGradient
                 colors={report.resolved ? [theme.colors.secondary1, theme.colors.primary1] : [theme.colors.red_dark, theme.colors.red]}
                 style={[styles.resolvedView, theme.shadowPropertiesLow]}
