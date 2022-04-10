25e7bc489d0f631fc5ddc5afd39f7d9e9e17a819
Eduardo <eduardomacielbr@gmail.com>
2022-04-10 15:48:59 -0300

    Mudança de servidor: local --> heroku

--- src/app.ts
+++ src/app.ts
@@ -11,5 +11,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(router)
 
 // process.env.PORT || 5000
+// 4000
+
 // Caso 'cê esteja rodando o servidor localmente (pelo PC), troca a porta pra 4000
-app.listen(4000, () => { console.log(`🚀 O servidor está rodando na porta ${process.env.PORT}`) })
+app.listen(process.env.PORT || 5000, () => { console.log(`🚀 O servidor está rodando na porta ${process.env.PORT}`) })
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 //https://dfl-detector-de-focos-de-lixo.herokuapp.com/
+//http://192.168.1.102:4000
 
 export const api = axios.create({
-    baseURL: 'http://192.168.1.102:4000',
+    baseURL: 'https://dfl-detector-de-focos-de-lixo.herokuapp.com/',
     timeout: 15 * 1000,
     timeoutErrorMessage: 'Timeout error',
 });
